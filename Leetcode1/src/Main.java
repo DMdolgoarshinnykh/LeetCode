fff9531ab5aca4d68de2df46a75889e616b5fa5e
Daniil <dmdoogoarshinnykh@miem.hse.ru>
2023-09-21 12:32:53 +0300

    first commit

---
+++ Leetcode1/src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Solution q = new Solution();
+        int[] array = new int[] {3,2,4};
+        q.twoSum(array,6);
+    }
+}
